b28fbe9ea9f82be8d09ef9fd410654ca1f0f14bc
Mohan <notcoldum@gmail.com>
2021-03-17 20:45:07 +0800

    Express skeleton & Homepage routing

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// Setting up dev environment
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen('3000', () => {
+    console.log('Listening on port 3000...')
+    console.log(__dirname)
+});
