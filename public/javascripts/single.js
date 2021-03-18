3e5249eb148dc78c31b4ef2dcca24cdbc485e79d
Mohan <notcoldum@gmail.com>
2021-03-18 16:22:24 +0800

    Single-user Page Frontend Skeleton

--- public/javascripts/single.js
+++ public/javascripts/single.js
@@ -3,6 +3,7 @@ var video = document.querySelector("#videoElement");
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(function (stream) {
         video.srcObject = stream;
+        video.muted = true;
     })
     .catch(function (error) {
         console.log('Failed to get local stream', error);
