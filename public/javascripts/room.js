3ab838ffc6916cd90c5858d856b220d876102b98
Mohan <notcoldum@gmail.com>
2021-03-19 13:46:56 +0800

    Uninstall peerjs on server side; Modify disconnection handler; Add comments to peer call process

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -1,3 +1,21 @@
+/*
+    Peer.js & Socket.io Logic:
+        (1) When the first user joins the room:
+            It emits 'join-room' event to server, where socket joins the channel specified by roomId.
+            The socket then emits 'user-connected' to everyone in the channel (in this case, no one).
+        
+        (2) When a new user A joins the room:
+            It emits 'join-room' event to server, where socket joins the channel specified by roomId.
+            The socket then emits 'user-connected' to everyone in the channel.
+            In this case, everyone in the room channel should receive the 'user-connected' emitted by socket A.
+            Let B denote one of such user.
+            B then starts a peer.call() to A: This is my video stream.
+            After doing so, B waits for A's video stream - mediaConnection.on('stream').
+            When A receives B's call, A answers the call with its video stream - mediaConnection.answer(stream).
+            Finally, B receives A's video stream - mediaConnection.on('stream').
+*/
+
+
 // socket.io and peer.js initialization
 const socket = io();
 const peer = new Peer();
@@ -23,11 +41,10 @@ const callNewUser = (username, peerId, stream) => {
     // Emitted when either you or the remote peer closes the media connection.
     // *Firefox does not yet support this event.
     mediaConnection.on('close', () => {
-        // Show stream in some <video> element.
         console.log('a user disconnected!')
         const video2 = document.querySelector("#videoElement2");
         // video2.remove();
-        video2.srcObject = '';
+        // video2.srcObject = '';
         const user2 = document.querySelector("#user2");
         user2.innerHTML = 'Disconnected';
     });
@@ -66,6 +83,18 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             });
 
             peerCalls[mediaConnection.peer] = mediaConnection;
+
+            // Emitted when either you or the remote peer closes the media connection.
+            // *Firefox does not yet support this event.
+            mediaConnection.on('close', () => {
+                console.log('a user disconnected!')
+                const video2 = document.querySelector("#videoElement2");
+                // video2.remove();
+                // video2.srcObject = '';
+                const user2 = document.querySelector("#user2");
+                user2.innerHTML = 'Disconnected';
+            });
+
         })
 
     })
