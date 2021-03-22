2758f1a2a6506f80f5832a4d107d79561ea968e6
Mohan <notcoldum@gmail.com>
2021-03-23 02:35:27 +0800

    Integrated Web Speech API to single-user testing

--- public/javascripts/single.js
+++ public/javascripts/single.js
@@ -1,3 +1,38 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+
+var recognition = new SpeechRecognition();
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = true;
+recognition.maxAlternatives = 1;
+
+recognition.start();
+
+recognition.onstart = () => {
+    console.log('Recognition starts');
+}
+recognition.onresult = function (event) {
+    var color = event.results[0][0].transcript;
+    document.querySelector('#result').textContent = color;    // text content
+    console.log('Confidence: ' + event.results[0][0].confidence);
+}
+
+recognition.onspeechend = function () {
+    console.log('on speech end');
+    recognition.stop();
+    setTimeout(() => {
+        recognition.start();
+    }, 400);
+}
+
+recognition.onerror = function (event) {
+    document.querySelector('#result').textContent = 'Error occurred in recognition: ' + event.error;
+    recognition.stop();
+    setTimeout(() => {
+        recognition.start();
+    }, 400);
+}
+
 const video = document.querySelector("#videoElement");
 
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
