cc331123d7d17ba37e4e7886a53cc0d1f33f1f71
Mohan <notcoldum@gmail.com>
2021-03-29 11:14:36 +0800

    making random usernames less janky

--- public/javascripts/join.js
+++ public/javascripts/join.js
@@ -1,6 +1,13 @@
 const dice = document.querySelector('#dice');
-const adjs = ['Grumpy', 'Old', 'Angry', 'Silent'];
-const animals = ['Cat', 'Dog', 'Duck', 'Chicken'];
+
+// Google Search adjectives for personality
+const adjs = ['Ambitious', 'Sincere', 'Responsible', 'Sophisticated', 'Outspoken', 'Considerate', 'Modest', 'Eloquent', 
+    'Energetic', 'Thoughtful', 'Obliging', 'Sociable', 'Assertive', 'Attentive', 'Outgoing', 'Pleasant', 'Gentle', 
+    'Courteous', 'Punctual', 'Trustworthy', 'Cheerful', 'Easy-going', 'Well-behaved', 'Calm'];
+
+// https://list25.com/25-most-popular-animals-on-google-search/
+const animals = ['Dog', 'Cat', 'Horse', 'Chicken', 'Fish', 'Bear', 'Bird', 'Shark', 'Snake', 'Pig', 'Lion', 'Turkey',
+    'Wolf', 'Spider', 'Rabbit', 'Duck', 'Deer', 'Cow', 'Monkey', 'Lobster', 'Ape', 'Pony', 'Eagle', 'Dolphin', 'Bison'];
 
 dice.addEventListener('click', (event) => {
     event.preventDefault();
